8530f711e6ffcc920280a88f39d1cb8e22ecb32c
prab0t <iprab0t@gmail.com>
2024-07-01 11:19:02 +0500

    CLR

---
+++ HW21 - CLR/CLR.sql
@@ -0,0 +1,38 @@
+exec sp_configure 'clr enabled', 1;  
+exec sp_configure 'clr strict security', 0;  
+GO 
+RECONFIGURE;  
+
+ALTER DATABASE WideWorldImporters SET TRUSTWORTHY ON;
+
+CREATE ASSEMBLY CLRFunctions FROM 'C:\SQLServerCLRSortString.dll'  
+WITH PERMISSION_SET = SAFE;
+GO 
+
+CREATE FUNCTION dbo.SortString    
+(    
+ @name AS NVARCHAR(255)    
+)     
+RETURNS NVARCHAR(255)    
+AS EXTERNAL NAME CLRFunctions.CLRFunctions.SortString 
+GO 
+
+select * from sys.assemblies
+
+CREATE TABLE testSort (data VARCHAR(255)) 
+GO
+
+INSERT INTO testSort VALUES('apple,pear,orange,banana,grape,kiwi') 
+INSERT INTO testSort VALUES('pineapple,grape,banana,apple') 
+INSERT INTO testSort VALUES('apricot,pear,strawberry,banana') 
+INSERT INTO testSort VALUES('cherry,watermelon,orange,melon,grape') 
+
+SELECT data, dbo.sortString(data) as sorted FROM testSort 
+
+
+DROP FUNCTION dbo.SortString  
+GO 
+DROP ASSEMBLY CLRFunctions 
+GO 
+DROP TABLE testSort 
+GO 
